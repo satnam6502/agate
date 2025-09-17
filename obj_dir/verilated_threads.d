48b458c5ca2fd93882e65cbede1e4586dc1203c9
Satnam Singh <satnam6502@gmail.com>
2025-09-17 13:55:57 -0700

    Remove binary files

--- obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
-  /Users/satnam/verilator/include/verilated_threads.cpp \
-  /Users/satnam/verilator/include/verilatedos.h \
-  /Users/satnam/verilator/include/verilated_threads.h \
-  /Users/satnam/verilator/include/verilated.h \
-  /Users/satnam/verilator/include/verilated_config.h \
-  /Users/satnam/verilator/include/verilated_types.h \
-  /Users/satnam/verilator/include/verilated_funcs.h
--- obj_dir/verilated_timing.d
+++
@@ -1,7 +0,0 @@
-verilated_timing.o: /Users/satnam/verilator/include/verilated_timing.cpp \
-  /Users/satnam/verilator/include/verilated_timing.h \
-  /Users/satnam/verilator/include/verilated.h \
-  /Users/satnam/verilator/include/verilated_config.h \
-  /Users/satnam/verilator/include/verilatedos.h \
-  /Users/satnam/verilator/include/verilated_types.h \
-  /Users/satnam/verilator/include/verilated_funcs.h
